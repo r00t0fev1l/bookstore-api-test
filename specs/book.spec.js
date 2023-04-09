d0aa3de227a579573390fb73597d577bbad06076
Tester <hola@hola.hola>
2023-04-09 22:37:43 +0500

    book api validation schema

--- specs/book.spec.js
+++ specs/book.spec.js
@@ -1,8 +1,15 @@
 import config from "../framework/config";
 import user from "../framework/services";
-
+import {
+    bookCollectionSchema,
+    bookSchema,
+    isbnNotSuppliedShema,
+    shortBookCollectionSchema
+} from "../framework/book.schema";
+import {expectToMatchSchema, setup} from "jest-json-schema-extended";
 let userId = null;
 let bearer = null;
+setup()
 
 describe('Book API', () => {
 
@@ -18,33 +25,35 @@ describe('Book API', () => {
 
     test('Book collection is not empty', async () => {
         const res = await user.getBooks();
-        //console.log(res.body.books);
         expect(res.body.books?.length).toBeGreaterThan(0)
+        expectToMatchSchema(res.body, bookCollectionSchema)
     })
 
     test( 'Add Books', async () => {
         const collectionOfIsbns = ["9781449325862", "9781449331818", "9781449337711"].map(value => ({isbn: value}))
         const res = await user.addBooks(collectionOfIsbns, userId, bearer);
         expect(res.body.books?.length).toEqual(3)
+        expectToMatchSchema(res.body, shortBookCollectionSchema)
     })
 
     describe.each(["9781449325862", "9781449331818", "9781449337711"])("Get book info", (isbn) => {
         test(`Get info ${isbn}`, async () => {
             const res = await user.getBook(isbn);
             expect(res.body.isbn).toEqual(isbn)
+            expectToMatchSchema(res.body, bookSchema)
         })
     })
 
     test.each( 'Replace Book', async () => {
         const res = await user.replaceBook("9781449325862", "9781593275846", userId);
-        console.log(res.body);
         expect(res.body.books?.length).toEqual(3)
+        expectToMatchSchema(res.body, bookSchema)
     })
 
     test( 'Replace with unexisted book', async () => {
         const res = await user.replaceBook("9781449325862", "1111111111111", userId);
-        console.log(res.body);
         expect(res.body.code).toEqual("1205")
+        expectToMatchSchema(res.body, isbnNotSuppliedShema)
     })
 
     describe.each(["9781449325862", "9781449331818", "9781449337711"])("Delete ISBN", (isbn) => {
